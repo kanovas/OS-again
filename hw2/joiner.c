184ef22bc8971ef790417b373b029c80d122afe5
kanovas <kano.vas.2.0@gmail.com>
2015-10-18 16:45:02 +0300

    hw2 finished

--- hw2/joiner.c
+++ hw2/joiner.c
@@ -1,26 +1,38 @@
 #include <sys/syscall.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 
 #define PERMS 0666
 
+int print_err(char* error, int code) {
+  write(2, error, strlen(error));
+  return code;
+}
+
 int main(int argc, char* argv[]) {
+  
+  
+  char* error0 = "Usage: join input_file1 input_file2 output_file\n";
+  char* error1 = "Unable to open or create file due to permissions\n";
+  char* error2 = "Read/write error\n"; 
   
   char buf[BUFSIZ];
   int n, m, input_file, output_file;
   
-  //TODO: check number of arguments
+  if (argc != 4) {
+    return print_err(error0, 1);
+  }
   
   output_file = creat(argv[3], PERMS);
   if (output_file < 0) {
-    //TODO: report error to 2
-    return 1;
+    return print_err(error1, 2);
   }
   
+  //first file
   input_file = open(argv[1], O_RDONLY | O_CREAT, PERMS); 
   if (input_file < 0) {
-    //TODO: report error to 2
-    return 1;
+    return print_err(error1, 2);
   }
   
    while ((n = read(input_file, buf, BUFSIZ)) > 0) {
@@ -28,14 +40,16 @@ int main(int argc, char* argv[]) {
        n -= m;
      }
      if (n != 0 || m == -1) {
-       return 2;
+       return print_err(error2, 3);
       }
-    }
+    }   
+  close(input_file);
   
+  
+  //second file
   input_file = open(argv[2], O_RDONLY | O_CREAT, PERMS); 
   if (input_file < 0) {
-    //TODO: report error to 2
-    return 1;
+    return print_err(error1, 2);
   }
   
   while ((n = read(input_file, buf, BUFSIZ)) > 0) {
@@ -43,7 +57,7 @@ int main(int argc, char* argv[]) {
       n -= m;
     }
     if (n != 0 || m == -1) {
-      return 2;
+      return print_err(error2, 3);
     }
   }
   
