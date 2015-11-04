43bc5a2b4f34ac969dee2424315fe95d71e2b637
kanovas <kano.vas.2.0@gmail.com>
2015-11-04 18:38:41 +0300

    1

--- hw6/semaphore.c
+++ hw6/semaphore.c
@@ -1,8 +1,11 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
 #define QUEUE_SIZE 1024
 
 struct queue {
@@ -37,15 +40,16 @@ int main() {
   
   struct mymsgbuf {
     long mtype;
-    
-    char operation; //'p', 'v', 'f'
-    int pid;
+    struct inf {
+      char operation; //'p', 'v'
+      int pid;
+    } info;
   } data, answer;
   
   struct mymsgbuf * mymsgbuf_init(struct mymsgbuf *buf, long mtype, char operation, int pid) {   
     buf->mtype = mtype;
-    buf->operation = operation;
-    buf->pid = pid;
+    buf->info.operation = operation;
+    buf->info.pid = pid;
     return buf;
   }
   //generating IPC key from filename for queue #0
@@ -61,7 +65,7 @@ int main() {
   } 
   
   void send_message(struct mymsgbuf *msg) {
-    int len = sizeof(*msg);
+    int len = sizeof(msg->info);
     if (msgsnd(msqid, msg, len, 0) < 0) {
       perror("Can\'t send message to queue1\n");
       msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
@@ -85,36 +89,35 @@ int main() {
 	  perror("Can\'t receive message from queue2\n");
 	  exit(-1);
       }  
-      printf("p operation made");
+      printf("p operation made\n");
       //verhogen
       send_message(mymsgbuf_init(&data, 1, 'v', cur_pid));
-      printf("v operation made");
+      printf("v operation made\n");
       exit(0);
     } 
   } 
   
-  send_message(mymsgbuf_init(&data, 1, 'f', 0));
-  
   //start listening
   while (1) {
-    if (len = msgrcv(msqid, &data, maxlen, 1, 0) < 0) {
+    if (len = msgrcv(msqid, &data, maxlen, 1, IPC_NOWAIT) < 0 && errno != EAGAIN) {    //not blocking msgrcv
       perror("Can\'t receive message from queue3\n");
       exit(-1);
     }
-    if (data.operation == 'f') { //finish work 
-      msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL); //delete msg queue
+    if (waitpid(-1, NULL, WNOHANG) < 0) {    //children are dead, finish work
+      msgctl(msqid, IPC_RMID, NULL);        //delete msg queue
       exit(0);
     } 
-    else if (data.operation == 'p') {
+    
+    if (data.info.operation == 'p') {
       if (s == 0) {
-	queue_push(&clients, data.pid);
+	queue_push(&clients, data.info.pid);
       }
       else {
 	s--;
-	send_message(mymsgbuf_init(&answer, data.pid, 'p', 0));
+	send_message(mymsgbuf_init(&answer, data.info.pid, 'p', 0));
       }
     }
-    else if (data.operation == 'v') {
+    else if (data.info.operation == 'v') {
       if (s == 0) {
 	s++;
 	if (queue_size(&clients) > 0) {
