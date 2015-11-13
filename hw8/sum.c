c47b8192628534f24236df59bb3ad5adcf1e8dbe
kanovas <kano.vas.2.0@gmail.com>
2015-11-13 23:15:16 +0300

    working

--- hw8/sum.c
+++ hw8/sum.c
@@ -7,60 +7,72 @@
 
 #define n 3
 #define m 3
-#define nThreads 3
 
 void *input(), *sum(), *output();
 int m1[n*m], m2[n*m], result[n*m];
+int new_matrices, new_result, i, j;
 sem_t mutex1, mutex2;
 
 void *input() {
-  int i, j;
+ 
   while(1) {
-    sem_wait(&mutex1); //waiting for matrices
-    printf("Input matrix #1\n");
-    for (i = 0; i < n; i++)
-      for (j = 0; j < m; j++)
-        scanf("%d", &m1[i*m + j]);
+    if (!new_matrices && !new_result) {
+      sem_wait(&mutex1); //waiting for matrices
+      printf("Input matrix #1\n");
+      for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+          scanf("%d", &m1[i*m + j]);
     
-    printf("Input matrix #2\n");
-    for (i = 0; i < n; i++)
-      for (j = 0; j < m; j++)
-        scanf("%d", &m2[i*m + j]);
-    sem_post(&mutex1); //opening matrices
+      printf("Input matrix #2\n");
+      for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+          scanf("%d", &m2[i*m + j]);
+      new_matrices = 1;
+      sem_post(&mutex1); //opening matrices
+    }
   }
 }
 
 void *sum() {
-  int i, j;
+  
   while(1) {
-    sem_wait(&mutex2); //waiting for result
-    sem_wait(&mutex1); //waiting for matrices
-    for (i = 0; i < n; i++) 
-      for (j = 0; j < m; j++) 
-        result[i*m + j] = m1[i*m + j] + m2[i*m + j];
-    sem_post(&mutex1); //opening matrices
-    sem_post(&mutex2); //opening result
+    if (new_matrices && !new_result) {
+      sem_wait(&mutex2); //waiting for result
+      sem_wait(&mutex1); //waiting for matrices
+      for (i = 0; i < n; i++) 
+        for (j = 0; j < m; j++) 
+          result[i*m + j] = m1[i*m + j] + m2[i*m + j];
+      new_matrices = 0;
+      new_result = 1;
+      sem_post(&mutex1); //opening matrices
+      sem_post(&mutex2); //opening result
+    }    
   }
 }
 
 void *output(){
-  int i, j;
+  
   while(1) {
-    sem_wait(&mutex2); //waiting for result
-    printf("Sum result is: \n");
-    for (i = 0; i < n; i++) {
-      for (j = 0; j < m; j++)
-        printf("%d ", result[i*m + j]);
-      printf("\n");
+    if (new_result) {
+      sem_wait(&mutex2); //waiting for result
+      printf("Sum result is: \n");
+      for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+          printf("%d ", result[i*m + j]);
+        printf("\n");
+      }
+      new_result = 0;
+      sem_post(&mutex2); //opening result
     }
-    sem_post(&mutex2); //opening result
   }
 }
 
 int main() { 
-  pthread_t thread[nThreads];
+  pthread_t thread[3];
   sem_init(&mutex1, 0, 1);
   sem_init(&mutex2, 0, 2);
+  new_matrices = 0;
+  new_result = 0;
   int res = pthread_create(&thread[0], NULL, &input, NULL);
   if (res != 0) {
     perror("Can't create thread");
@@ -76,6 +88,10 @@ int main() {
     perror("Can't create thread");
     return -1;
   }
+  while (1) {
+    continue;
+  }
+  
   sem_destroy(&mutex1);
   sem_destroy(&mutex2);
   
